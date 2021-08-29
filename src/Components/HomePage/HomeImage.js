ea0b174b6ff7c486660a4df5c689342b4cf9a9c8
vinay vishwakarma <vinay.myjob@gmail.com>
2021-08-29 20:16:36 +0530

    change Hero Image url to Local name

--- src/Components/HomePage/HomeImage.js
+++ src/Components/HomePage/HomeImage.js
@@ -2,10 +2,7 @@ import React from "react";
 const HomeImage = () => {
   return (
     <div className="home-image col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center">
-      <img
-        src="https://lh3.googleusercontent.com/114dM3K6iRibiDKWL5ypIwHzmn6oY0UuV30lqcQZGCFpaWFDJghw3RGYNTRPxVhnIl0blmv1CwCNDTcEt_PMs4OfSqeYGHKAezjwpl3Q1Z64zJ4LrhLQcVoLC117EJU_l7AvHZpzDyBjS-UnSdUJW2uYYxRHGo9C_js26c0PxGcNgFqYtr9df_NeTDlFJxRP7zY-qVQ3xib0WjqCC2DENAuO_xCT86qwekfneXxvoBNBsCy7kGrY2TmZk0etT1N7t9TCU_7MqOluX4leAa79M6zz51QzeI8HraQkTSIIqm5S25rqGZgD78vlR_9PFHpq03XxBthlXsJSuUSRxa2Ts5LDSq7iMVsYo8c66UzjWUD-lNEB2n3-3DAiNB0T8_NislDcv7v40ydigi5Uvl-DL1s9oq5LxsLq04QOmpCc22lhefmxf6bN-P9xOH7zyGTSis74o5d0SIb-FsGKvsPGCc5-ZbXjnEj3nrPQbqgKpRkZU4Uypwki4QL8yawptWXOsfxIcQuMUAJSYVE8xwKUFZmI7j2uYjtftgXbxm3DJH17o6vFMtK_5SemBzH0sr-u3hdi67wF2YP01fi7_nbrhzP3AkDpNlWbTaHPBKUN5Cd6oWeD5Y5pVSEoLkIIX4T2hLDCJppxu5DEtP8ovAZS0TqzLmcHzurOFvg7AKaxgZz--3pr83hVZCyCnEIPzTvEsOZFSA1j89bgOY8HrqcXoNs=w810-h463-no?authuser=4"
-        alt="hero-image"
-      />
+      <img src="images/HeroImage.svg" alt="heroImage" />
     </div>
   );
 };
